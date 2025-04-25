476e8b265f2804db4e6b22d7aaef6c28bd61f98d
westfalltv <westfalltv@gmail.com>
2025-04-25 11:29:44 +0900

    첫 커밋

---
+++ board-front/board-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <hearder className="App-header">
+        <h1>Hello World</h1>
+      </hearder>
+    </div>
+  );
+}
+
+export default App;
