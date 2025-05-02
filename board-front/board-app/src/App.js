3d41108133cec403f032a837463526e2b7486084
westfalltv <westfalltv@gmail.com>
2025-05-02 14:40:24 +0900

    리액트 crud

--- board-front/board-app/src/App.js
+++ board-front/board-app/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import TOC from "./components/TOC"
-import Content from './components/Content'
+import ReadContent from './components/ReadContent'
 import Subject from "./components/Subject"
 import Control from "./components/Control"
+import CreateContent from "./components/CreateContent"
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'read',
+      mode: 'create',
       subject: {title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'welcome', desc:'Hello,React!!'},
       contents: [
@@ -21,10 +23,11 @@ class App extends Component {
   }
   render() {
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
       var i =0;
       while(i< this.state.contents.length){
@@ -36,6 +39,22 @@ class App extends Component {
         }
         i=i+1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id+1;
+        // this.state.contents.push(
+        //  {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents:_contents
+      })
+        console.log(_title, _desc)
+      }.bind(this)}></CreateContent>
     }
     return (
       <div className="App">
@@ -61,8 +80,8 @@ class App extends Component {
             mode:_mode
           });
         }.bind(this)}></Control>
-        <Content title={_title} desc={_desc}></Content>
-      </div>
+        {_article}
+        </div>
     );
   }
 } 
