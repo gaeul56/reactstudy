4d44c0e0324af71b7a1726978d00872f9a68ded8
westfalltv <westfalltv@gmail.com>
2025-05-12 10:09:24 +0900

    reactstudy

--- basicapp/src/03/MyDiv2.js
+++ basicapp/src/03/MyDiv2.js
@@ -1,10 +1,12 @@
 import MyDiv3 from "./MyDiv3"
-export default function MyDiv2() {
+export default function MyDiv2(probs) {
   return (
     <div className="flex flex-col p-5 m-10
                         w-3/4 h-3/4
                         bg-lime-700 text-white">
-        <div className="w-full flesx justify-start">myDiv2</div>      
+        <div className="w-full flesx justify-start">
+           { `${probs.dn1} > ${probs.dn2}` }
+        </div>      
         <MyDiv3 />
     </div>
   )
