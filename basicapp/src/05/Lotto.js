eb958f9da6e5c74ceaa61b73533df9d1d2358289
westfalltv <westfalltv@gmail.com>
2025-05-13 09:47:22 +0900

    lotto

--- basicapp/src/05/Lotto.js
+++ basicapp/src/05/Lotto.js
@@ -1,6 +1,9 @@
 import TailButton from "../UI/TailButton"
+import Ball from './Temp';
+import {useState} from "react";
 
 export default function Lotto() {
+  const [tags, setTags] = useState();
   const handleOK = () => {
     let arr = [];
 
@@ -13,13 +16,20 @@ export default function Lotto() {
     let bonus = arr.splice(-1);
     arr.sort((a, b) => (a-b));
     console.log(arr, bonus)
+
+    let tm = arr.concat(bonus);
+    tm = tm.map( item => <Ball n={item} leu={`b${item}`}/>);
+
+    tm.splice(6, 0, <span className="text-3xl mx-2" key="sp">+</span>);
+    setTags(tm);
  }
 
   return (
     <div className="w-full flex flex-col
                     justify-center items-center">
-        <div>
-            ball
+
+        <div className="m-10">
+            {tags}
         </div>
         <div>
             <TailButton caption='로또번호생성' bcolor='orange' handleClick={handleOK}></TailButton>
